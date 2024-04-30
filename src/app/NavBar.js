35c35ab9a963f154cfeef61f583247775a683505
abhis12321 <abhisheksingh2359328@gmail.com>
2024-04-30 07:07:57 +0530

    job listing page added

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -15,21 +15,21 @@ export default function NavBar() {
     return (
         <div className='leading-[3rem] flex items-center justify-between px-7 py-1 bg-gradient-to-b from-gray-950 to-gray-800'>
             <div className="text-3xl font-serif leading-[3.4rem] bg-gradient-to-r from-pink-500 to-violet-600 bg-clip-text text-transparent font-extrabold">Japan</div>
-            <div className="right-navbar hidden md:flex flex-col md:flex-row lg:gap-4 bg-gradient-to-r from-gray-800 to-slate-950 md:[background:none] min-w-[200px] min-h-[100vh] md:min-h-fit fixed md:static top-0 right-0">
+            <div className="right-navbar hidden md:flex flex-col md:flex-row lg:gap-4 bg-gradient-to-r from-slate-800 to-gray-950 md:[background:none] border-l-2 border-gray-700 md:border-none min-w-[200px] min-h-[100vh] md:min-h-fit fixed md:static top-0 right-0">
                 <div className="text-4xl hover:text-red-800 text-center cursor-pointer mt-3" onClick={handleRight}>
                     <FontAwesomeIcon icon={faXmark} size='sm' className="hidden cutX" />
                 </div>
                 <div className="w-full md:w-fit flex items-center justify-center">
-                    <Link href={''} className='hover:bg-gray-950/50 md:hover:bg-red-950/20 md:hover:shadow-[0_0_3px_white] text-center w-full md:min-w-28 md:px-4 md:rounded-xl'>Home</Link>
+                    <Link href={''} className='hover:bg-cyan-950/40 md:hover:bg-red-950/20 md:hover:shadow-[0_0_3px_white] text-center w-full md:min-w-28 md:px-4 md:rounded-xl'>Home</Link>
                 </div>
                 <div className="w-full md:w-fit flex items-center justify-center">
-                    <Link href={''} className='hover:bg-gray-950/50 md:hover:bg-red-950/20 md:hover:shadow-[0_0_3px_white] text-center w-full md:min-w-28 md:px-4 md:rounded-xl'>Job Listings</Link>
+                    <Link href={'/job-listings'} className='hover:bg-cyan-950/40 md:hover:bg-red-950/20 md:hover:shadow-[0_0_3px_white] text-center w-full md:min-w-28 md:px-4 md:rounded-xl'>Job Listings</Link>
                 </div>
                 <div className="w-full md:w-fit flex items-center justify-center">
-                    <Link href={'/about'} className='hover:bg-gray-950/50 md:hover:bg-red-950/20 md:hover:shadow-[0_0_3px_white] text-center w-full md:min-w-28 md:px-4 md:rounded-xl'>About Japan</Link>
+                    <Link href={'/about'} className='hover:bg-cyan-950/40 md:hover:bg-red-950/20 md:hover:shadow-[0_0_3px_white] text-center w-full md:min-w-28 md:px-4 md:rounded-xl'>About Japan</Link>
                 </div>
                 <div className="w-full md:w-fit flex items-center justify-center">
-                    <Link href={''} className='hover:bg-gray-950/50 md:hover:bg-red-950/20 md:hover:shadow-[0_0_3px_white] text-center w-full md:min-w-28 md:px-4 md:rounded-xl'>Contact</Link>
+                    <Link href={''} className='hover:bg-cyan-950/40 md:hover:bg-red-950/20 md:hover:shadow-[0_0_3px_white] text-center w-full md:min-w-28 md:px-4 md:rounded-xl'>Contact</Link>
                 </div>
             </div>
 
---
+++ src/app/job-listings/page.js
@@ -0,0 +1,179 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+const preJobs = [
+    {
+        "company_name": "Prenlyn Automotive Group",
+        "formatted_relative_time": "3 days ago",
+        "id": "bcfebc9d0c9a0d3b",
+        "link": "/job/bcfebc9d0c9a0d3b?locality=us",
+        "locality": "us",
+        "location": "Springfield, PA 19064",
+        "pub_date_ts_milli": 1714107600000,
+        "salary": {},
+        "title": "Midas Store Manager, $900 - $1,200/week Plus Bonuses!"
+    },
+    {
+        "company_name": "RF INSTALLATIONS LLC",
+        "formatted_relative_time": "30+ days ago",
+        "id": "f3121153967b1caa",
+        "link": "/job/f3121153967b1caa?locality=us",
+        "locality": "us",
+        "location": "Seattle, WA",
+        "pub_date_ts_milli": 1711083600000,
+        "salary": {
+            "max": 65000,
+            "min": 0,
+            "type": "yearly"
+        },
+        "title": "Field Manager"
+    },
+    {
+        "company_name": "TLC - The Lube Center",
+        "formatted_relative_time": "18 days ago",
+        "id": "f82751ef276942ea",
+        "link": "/job/f82751ef276942ea?locality=us",
+        "locality": "us",
+        "location": "Roanoke, VA 24018",
+        "pub_date_ts_milli": 1712811600000,
+        "salary": {},
+        "title": "Service Manager/Technician"
+    },
+    {
+        "company_name": "1-Stop",
+        "formatted_relative_time": "27 days ago",
+        "id": "007c1eaddcee8bf5",
+        "link": "/job/007c1eaddcee8bf5?locality=us",
+        "locality": "us",
+        "location": "Mount Morris, MI 48458",
+        "pub_date_ts_milli": 1711947600000,
+        "salary": {
+            "max": 20,
+            "min": 20,
+            "type": "hourly"
+        },
+        "title": "Store Manager"
+    },
+    {
+        "company_name": "Fatface Corporation",
+        "formatted_relative_time": "30+ days ago",
+        "id": "e3a240d73a2c7b16",
+        "link": "/job/e3a240d73a2c7b16?locality=us",
+        "locality": "us",
+        "location": "Kennebunkport, ME 04046",
+        "pub_date_ts_milli": 1709100000000,
+        "salary": {},
+        "title": "Assistant Store Manager"
+    },
+    {
+        "company_name": "EZ Stop",
+        "formatted_relative_time": "28 days ago",
+        "id": "ade831bba9d0e68b",
+        "link": "/job/ade831bba9d0e68b?locality=us",
+        "locality": "us",
+        "location": "Newport, TN 37821",
+        "pub_date_ts_milli": 1710219600000,
+        "salary": {},
+        "title": "EZ Stop Store Manager"
+    }
+]
+
+export default function Page() {
+  const [jobs, setJobs] = useState(preJobs);
+  const [role, setRole] = useState("manager");
+
+  useEffect(() => {
+    console.log("rendering..");
+  }, []);
+
+  const searchJobs = async (e) => {
+    e.preventDefault();
+    console.log("seraching...");
+    const url = `https://indeed12.p.rapidapi.com/jobs/search?query=${role}&location=japan`;
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "9e63764c0fmsh8714f3982a78195p1af512jsnca5b2fdf24e8",
+        "X-RapidAPI-Host": "indeed12.p.rapidapi.com",
+      },
+    };
+
+    try {
+      fetch(url, options)
+        .then((res) => res.json())
+        .then((data) => setJobs(data.hits))
+        .then((e) => console.log(jobs));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const logJobs = () => {
+    console.log(jobs);
+  }
+  
+  return (
+    <div className="py-4 flex flex-col gap-4">
+      <form
+        className="flex gap-3 py-2 px-3 w-[98%] max-w-[700px] bg-gray-800 mx-auto rounded"
+        onSubmit={searchJobs}
+      >
+        <input
+          type="text"
+          value={role}
+          onChange={(e) => setRole(e.target.value)}
+          placeholder="Search a Role"
+          className="py-2 px-4 flex-1 bg-teal-900/20 text-center text-gray-200 outline-none shadow-[0_0_3px_gray] focus:shadow-[0_0_3px_cyan] rounded"
+        />
+        <input
+          type="submit"
+          value={"search"}
+          className="hidden xs:block py-2 px-4 cursor-pointer shadow-[0_0_3px_cyan] hover:bg-cyan-950/80 active:bg-slate-950 rounded"
+        />
+      </form>
+
+      <h1 className="text-center py-4 px-8 mb-2 rounded-md drop-shadow-[0_0_1px_red] text-4xl sm:text-5xl font-sgray font-exyellod sm:font-extrabold text-transparent bg-clip-text bg-gradient-to-l from-gray-600 to-yellow-900 w-fit" onClick={logJobs}>
+        Job listings
+      </h1>
+
+      <div className="flex flex-col gap-5 p-4">
+        {jobs?.map((job, index) => (
+          <div className="flex flex-col bg-slate-800 p-8 rounded-lg shadow-[0_0_5px_white] hover:shadow-[0_0_8px_red]" key={index}>
+            <h1 className="text-gray-400 font-semibold text-lg">
+              Company Name :
+              <span className="text-gray-100 text-xl font-bold">
+                {job.company_name}
+              </span>
+            </h1>
+            <h1 className="text-gray-400 font-semibold text-lg">
+              Job title :
+              <span className="text-gray-100 text-xl font-bold">
+                {job.title}
+              </span>
+            </h1>
+            <h1 className="text-gray-400 font-semibold text-lg">
+              Locaction :
+              <span className="text-gray-100 text-xl font-bold">
+                {job.location}, <span>{job.locality}</span>
+              </span>
+            </h1>
+            <h1 className="text-gray-400 font-semibold text-lg">
+              published :
+              <span className="text-gray-100 text-xl font-bold">
+                {job.formatted_relative_time}
+              </span>
+            </h1>
+            <p className="text-gray-400 font-semibold text-lg">
+              Salary :
+              <span className="text-gray-100 text-xl font-bold">
+                {job.salary.type
+                  ? "$" + job.salary.min + " to $" + job.salary.max
+                  : "Not disclosed"}
+              </span>
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
